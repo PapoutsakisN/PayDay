f609e3c0a0c430bb854d605904bf56e359902a93
Nikolaos Papoutsakis <nick.papoutsakis4@gmail.com>
2021-10-06 18:49:54 +0300

    Create MovetoDB.java

---
+++ PhaseA/src/model/MovetoDB.java
@@ -0,0 +1,21 @@
+package model;
+
+/**
+ *
+ * @author csd3035
+ */
+public class MovetoDB extends MessageCard {
+
+    /**
+     * constructor: Constructs a new MovetoDealorBuyer message card
+     */
+    public MovetoDB() {
+    }
+    /**
+     * transformer: Change players position to the nearest Buyer position of the board
+     * postcondition: New position on board for the player
+     * @param p is the player that drew this card
+     */
+    public void Action(Player p) {
+    }
+}
