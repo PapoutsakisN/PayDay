3e5cb2fc1c823614b73824734e23b91c42863dbf
Nikolaos Papoutsakis <nick.papoutsakis4@gmail.com>
2021-10-06 18:49:51 +0300

    Create MessagePos.java

---
+++ PhaseA/src/model/MessagePos.java
@@ -0,0 +1,14 @@
+
+package model;
+
+
+public class MessagePos extends BoardPosition {
+    private boolean twocards;
+    public MessagePos(boolean b){
+        this.twocards = b;
+    }
+    
+    public void toDo(Player p){
+        
+    }
+}
