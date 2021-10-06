752e6c3d35c03036fcb3e49a538267882dce3f85
Nikolaos Papoutsakis <nick.papoutsakis4@gmail.com>
2021-10-06 18:50:09 +0300

    Create StartPosition.java

---
+++ PhaseA/src/model/StartPosition.java
@@ -0,0 +1,25 @@
+
+package model;
+
+/**
+ *
+ * @author csd3035
+ */
+public class StartPosition extends BoardPosition{
+
+    /**
+     * constructor: Constructs the start position
+     */
+    public StartPosition(){
+        
+    }
+
+    /**
+     * accessors: Return position 0 as the start position
+     * postcondition: Return position 0 as the start position
+     * @return position 0 as the start position
+     */
+    public int getSpos(){
+        return 0;
+    }
+}
