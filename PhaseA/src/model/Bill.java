25380db56a248e20800452c0a11e905c52b897c4
Nikolaos Papoutsakis <nick.papoutsakis4@gmail.com>
2021-10-06 18:48:59 +0300

    Create Bill.java

---
+++ PhaseA/src/model/Bill.java
@@ -0,0 +1,23 @@
+package model;
+
+/**
+ *
+ * @author csd3035
+ */
+public class Bill extends MessageCard {
+
+    /**
+     * constructor: Constructs a bill message card
+     */
+    public Bill(){
+    
+    }
+
+    /**
+     * transformer: It adds the bill money to players billDept
+     * postcondition: Informed billDept of player
+     * @param p is the player who drew the bill card
+     */
+    public void Action(Player p) {
+    }
+}
