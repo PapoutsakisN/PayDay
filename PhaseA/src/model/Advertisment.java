0067ba3d3583a5ab4e48e1a4cf88273db30a8462
Nikolaos Papoutsakis <nick.papoutsakis4@gmail.com>
2021-10-06 18:48:57 +0300

    Create Advertisment.java

---
+++ PhaseA/src/model/Advertisment.java
@@ -0,0 +1,23 @@
+package model;
+
+/**
+ *
+ * @author csd3035
+ */
+public class Advertisment extends MessageCard {
+
+    String[] ads = new String[8];
+
+    /**
+     * constructor: Constructs a new Advertisment message card
+     */
+    public Advertisment() {
+
+    }
+    /**
+     * observer: It just prints a message (advertisment)
+     */
+    public void Action() {
+
+    }
+}
