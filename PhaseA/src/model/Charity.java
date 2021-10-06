708c92d2104a79fd997c95879a143d169daae8ec
Nikolaos Papoutsakis <nick.papoutsakis4@gmail.com>
2021-10-06 18:49:28 +0300

    Create Charity.java

---
+++ PhaseA/src/model/Charity.java
@@ -0,0 +1,25 @@
+package model;
+
+/**
+ *
+ * @author csd3035
+ */
+public class Charity extends MessageCard {
+
+    private int JackMoney;
+
+    /**
+     * constructor: Constructs a Charity message card
+     */
+    public Charity() {
+
+    }
+
+    /**
+     * transformer: Takes money from player and gives them to Jackpot
+     * postcondition: Informs players money and Jackpot money
+     * @param p is the player who drew the card 
+     */
+    public void Action(Player p) {
+    }
+}
