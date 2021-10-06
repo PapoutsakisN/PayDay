0a2159656c2f770af38ea405f8aae1417798a8bc
Nikolaos Papoutsakis <nick.papoutsakis4@gmail.com>
2021-10-06 18:50:21 +0300

    Create YardSale.java

---
+++ PhaseA/src/model/YardSale.java
@@ -0,0 +1,21 @@
+package model;
+
+/**
+ *
+ * @author csd3035
+ */
+public class YardSale extends BoardPosition {
+
+    /**
+     * constructor: constructs a Yardsale board position
+     */
+    public YardSale() {
+    }
+
+    /**
+     * transformer: Player rolls the dice and get top deal card with 100*dice discount 
+     * postcondition: Put deal card in player's deal cards
+     * @param p is the player stepped on yard sale board position
+     */
+    public void toDo(Player p){}
+}
