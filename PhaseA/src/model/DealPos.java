966d39fe0b5b06a2fa2eff9f7b51fa5c4a64c1ed
Nikolaos Papoutsakis <nick.papoutsakis4@gmail.com>
2021-10-06 18:49:33 +0300

    Create DealPos.java

---
+++ PhaseA/src/model/DealPos.java
@@ -0,0 +1,21 @@
+
+package model;
+
+/**
+ *
+ * @author csd3035
+ */
+public class DealPos extends BoardPosition {
+
+    /**
+     * constructor: Constructs a deal board position
+     */
+    public DealPos(){}
+
+    /**
+     * transformer: Player draw a deal card from deal card stack
+     * postcondition: Top deal card from table's deal card stack is transferred to player's deal card stack
+     * @param p is the player that stepped on the deal board position
+     */
+    public void toDo(Player p){}
+}
