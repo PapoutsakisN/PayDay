17514daaf5d65b588fb070cd0f6eef57831594eb
Nikolaos Papoutsakis <nick.papoutsakis4@gmail.com>
2021-10-06 18:49:59 +0300

    Create PaytheNeighbor.java

---
+++ PhaseA/src/model/PaytheNeighbor.java
@@ -0,0 +1,23 @@
+package model;
+
+/**
+ *
+ * @author csd3035
+ */
+public class PaytheNeighbor extends MessageCard {
+
+    /**
+     *constructor: Constructs a new PaytheNeighbor message card
+     */
+    public PaytheNeighbor() {
+    }
+
+    /**
+     * transformer: It takes money from player 1 that drew the card and gives them to player 2
+     * postcondition : It informs both players money
+     * @param p1 is player 1 who drew the card
+     * @param p2 is player 2 who will be payed
+     */
+    public void Action(Player p1, Player p2) {
+    }
+}
