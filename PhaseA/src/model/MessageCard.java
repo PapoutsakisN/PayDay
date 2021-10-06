5000a180f1b543d56e59c33c9e961f0eb5065af2
Nikolaos Papoutsakis <nick.papoutsakis4@gmail.com>
2021-10-06 18:49:48 +0300

    Create MessageCard.java

---
+++ PhaseA/src/model/MessageCard.java
@@ -0,0 +1,20 @@
+package model;
+
+/**
+ *
+ * @author csd3035
+ */
+public class MessageCard implements Card {
+    
+    /**
+     * constructor: Constructs a message card
+     */
+    public MessageCard() {
+
+    }
+
+    /**
+     * Classes that extend MessageCard will use it
+     */
+    public void Action(){}
+}
