45cbbef8e59ae3f7134f408e497529cefac8fd55
Nikolaos Papoutsakis <nick.papoutsakis4@gmail.com>
2021-10-06 19:00:00 +0300

    Create Test.java

---
+++ PhaseB/src/view/Test.java
@@ -0,0 +1,14 @@
+
+package view;
+
+/**
+ *
+ * @author csd3035
+ */
+public class Test {
+   public static void main(String[] args){
+    GUI game = new GUI();
+    game.InitializePane();
+   }
+}
+//den plironei
