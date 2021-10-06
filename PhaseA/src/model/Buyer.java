9cb760ed5fd57a93584de51c32373eda5fd301e3
Nikolaos Papoutsakis <nick.papoutsakis4@gmail.com>
2021-10-06 18:49:18 +0300

    Create Buyer.java

---
+++ PhaseA/src/model/Buyer.java
@@ -0,0 +1,22 @@
+package model;
+
+/**
+ *
+ * @author csd3035
+ */
+public class Buyer extends BoardPosition {
+    
+    /**
+     * constructor: Constructs a buyer board position
+     */
+    public Buyer(){
+    
+    }
+
+    /**
+     * transformer: Player gives a deal card and get the value , then loses the card 
+     * postcondition: Value of the deal card is added in player's money and deal card is moved from player to rejected stack 
+     * @param p is the player that stepped on buyer board position
+     */
+    public void toDo(Player p){}
+}
