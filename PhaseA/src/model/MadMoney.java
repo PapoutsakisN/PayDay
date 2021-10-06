1649b387d08dfbf9323faf03fb9af818da853e4d
Nikolaos Papoutsakis <nick.papoutsakis4@gmail.com>
2021-10-06 18:49:45 +0300

    Create MadMoney.java

---
+++ PhaseA/src/model/MadMoney.java
@@ -0,0 +1,23 @@
+package model;
+
+/**
+ *
+ * @author csd3035
+ */
+public class MadMoney extends MessageCard {
+
+    /**
+     * constructor: Constructs a new instance of MadMoney message card
+     */
+    public MadMoney() {
+    }
+
+    /**
+     * transformer: It takes money from player2 and add them to player1
+     * postcondition: Both Players money are informed about the changes
+     * @param p1 for player1
+     * @param p2 for player2
+     */
+    public void Action(Player p1, Player p2) {
+    }
+}
