4928fddb3f0f7e9ffd97fc052172df1ffe8ed369
Nikolaos Papoutsakis <nick.papoutsakis4@gmail.com>
2021-10-06 18:49:25 +0300

    Create CardStack.java

---
+++ PhaseA/src/model/CardStack.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author csd3035
+ */
+public class CardStack implements Card {
+
+    private Stack<Card> stack;
+
+    /**
+     * constructor: Constructs a stack of cards  
+     */
+    public CardStack() {
+
+    }
+
+    /**
+     * transformer: Initialize the stack with the necessary cards
+     * postcondition: Initialized stack of cards
+     */
+    public void InitializeCards() {
+
+    }
+
+    /**
+     * accessor: Returns the top card of the stack
+     * postcondition: Returns the card from the top of the stack for the player
+     * @return the top card of the stack (Card)
+     */
+    public Card DrawCard() {
+
+    }
+
+    /**
+     * observer: Returns if the card stack is empty
+     * postcondition: Returns if the card stack is empty
+     * @return true if the stack is empty of cards , false otherwise
+     */
+    public boolean isEmpty() {
+
+    }
+
+    /**
+     * transformer: Shuffle the stack of cards
+     * postcondition: Shuffled stack of cards
+     */
+    public void ShuffleStack() {
+
+    }
+}
