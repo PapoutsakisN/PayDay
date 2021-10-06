10e9f23d0c40ec5fa03f989fb64e877170c5f6b1
Nikolaos Papoutsakis <nick.papoutsakis4@gmail.com>
2021-10-06 18:55:52 +0300

    Create Table.java

---
+++ PhaseB/src/model/Table.java
@@ -0,0 +1,16 @@
+package model;
+
+/**
+ *
+ * @author csd3035
+ */
+public interface Table {
+
+    /**
+     * accessor: Return the month the month playing
+     * postcondition: Return the month playing
+     * @return the month playing
+     */
+    public int getMonth();
+
+}
